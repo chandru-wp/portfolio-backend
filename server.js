42924fcdc22c87a014f41225bb8e48a8f8aad06a
Chandru <ck6582293@gmail.com>
2026-01-04 15:25:39 +0530

    resume download error fix

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
+  'http://localhost:5175',
   'http://localhost:3000',
   'http://localhost:5001',
   'https://chandru-wp.github.io'
